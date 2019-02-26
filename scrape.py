c699d81b251e9f2b51213324a2e389ad4e09af34
Tianyuan Cai <tycai47@gmail.com>
2019-02-25 17:08:22 -0800

    finalized

--- scrape.py
+++ scrape.py
@@ -12,10 +12,9 @@ import re
 #print(soup.prettify())
 
 # Execution
-max_results = 100
+max_results = 10
 title_set = ["data+scientist", "data+analyst", "product+analyst"]
-#city_set = ['San+Francisco', 'Seattle', 'Portland', 'Los+Angeles', 'New+York','Chicago', 'Pittsburgh', 'Austin']
-city_set = ['San+Francisco']
+city_set = ["San+Francisco", "Seattle", "Portland", "Los+Angeles", "New+York"]
 columns = ["city", "company_name", "category", "job_title", "qualified", "tech_compatible", "major_compatible", "app_link", "indeed_link"]
 df = pd.DataFrame(columns = columns)
 
@@ -23,7 +22,7 @@ for title in title_set:
 	for city in city_set:
 		for start in range(0, max_results, 10):
 			jobs, postings, app_links, qualified, plang, majors, companies = [], [], [], [], [], [], []
-			page = requests.get('https://www.indeed.com/jobs?q='+title + '&l=' + str(city) + '&start=' + str(start))
+			page = requests.get("https://www.indeed.com/jobs?as_and=" + title + "&as_phr=&as_any=bachelor%2C+BA&as_not=&as_ttl=&as_cmp=&jt=all&st=&as_src=&salary=&radius=50&l="+ city + "&fromage=any&limit=10&sort=&psf=advsrch&start=" + str(start))
 			soup = BeautifulSoup(page.text, "html.parser")
 
 			for div in soup.find_all(name="div", attrs={"class":"row"}):
@@ -38,20 +37,20 @@ for title in title_set:
 
 				for a in div.find_all(name="a", attrs={"data-tn-element":"jobTitle"}):
 					# Jobs titles
-					jobs.append(''.join(a.text.strip()))
-					postings.append('http://indeed.com' + a['href'])
+					jobs.append("".join(a.text.strip()))
+					postings.append("https://indeed.com" + a["href"])
 
 					# Job listing detail page
-					job_page = requests.get('https://indeed.com' + a['href'])
+					job_page = requests.get("https://indeed.com" + a["href"])
 					job_soup = BeautifulSoup(job_page.text, "html.parser")
 
 					# Application link
 					application_links = job_soup.find_all(name = "a", attrs = {"class":"icl-Button icl-Button--primary icl-Button--md"})
-					application_links.sort(key = lambda x: x['href'], reverse = True)
+					application_links.sort(key = lambda x: x["href"], reverse = True)
 					if application_links:
 						for link in application_links:
-							if link['href'].find('promo/resume') == -1:
-								app_links.append(link['href'])
+							if link["href"].find("promo/resume") == -1:
+								app_links.append(link["href"])
 								break
 							else:
 								app_links.append("Unknown")
@@ -60,21 +59,21 @@ for title in title_set:
 
 					# Whether within 2 YOE and Bachlor
 					data = str(job_soup.findAll(text = True)).lower()
-					if (re.search('(bachelor|ba)[^a-z0-9]', data) is not None or re.search('(master|ma|phd)[^a-z0-9]', data) is None):
-						if re.search('[1-2].{1,10}(Y|y)ear', data) is not None:
+					if (re.search("(bachelor|ba)[^a-z0-9]", data) is not None or re.search("(master|ma|phd)[^a-z0-9]", data) is None):
+						if re.search("[1-2].{1,10}(Y|y)ear", data) is not None:
 							qualified.append("True")
 						else:
 							qualified.append("Need YOE")
-					elif re.search('[1-2].{1,10}(Y|y)ear', data) is not None:
+					elif re.search("[1-2].{1,10}(Y|y)ear", data) is not None:
 						qualified.append("Need Advanced Degree")
 					else:
 						qualified.append("False")
 
 					# Compatible technologies
-					plang.append("True") if re.search('(r|sql|python)[^a-z0-9]', data) is not None else plang.append("False")
+					plang.append("True") if re.search("(r|sql|python)[^a-z0-9]", data) is not None else plang.append("False")
 
 					# Compatible major
-					majors.append("True") if re.search('(economics)[^a-z0-9]', data) is not None else majors.append("False")
+					majors.append("True") if re.search("(economics)[^a-z0-9]", data) is not None else majors.append("False")
 
 			listings = [("job_title", jobs),
 						("company_name", companies),
@@ -92,4 +91,4 @@ for title in title_set:
 
 df = df[["city", "company_name", "category", "job_title", "qualified", "tech_compatible", "major_compatible", "app_link", "indeed_link"]]
 df = df.sort_values(by = ["qualified", "tech_compatible", "major_compatible"], ascending = [0, 0, 0])
-df.to_csv("H:/git_repo/job_scraping/output.csv", encoding='utf-8', index=False)
+df.to_csv("H:/git_repo/job_scraping/output.csv", encoding="utf-8", index=False)
