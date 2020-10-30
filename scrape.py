76d08a1be929bd040301dd7afa547f8743fd3634
tcai <tycai47@gmail.com>
2020-10-29 22:38:51 -0400

    added design notes

--- scrape.py
+++ scrape.py
@@ -1,102 +1,178 @@
 # -*- coding: utf-8 -*-
+"""
+We collect job description information from indeed.com
+Based on the user rating of compatibility and interest on each job description
+We personalize the recommendation of the jobs over time
+
+Application: this can be integrated into our current flask web app
+
+Oct 29, 2020
+Haowei Liu, Tianyuan Cai
+"""
+
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import time
 import re
-
-# test
-#URL = "https://www.indeed.com/jobs?q=data+scientist&l=New+York&start=10"
-#page = requests.get(URL)
-#soup = BeautifulSoup(page.text, "html.parser")
-#print(soup.prettify())
+import itertools
 
 # Execution
-max_results = 100
-title_set = ["data+scientist", "data+analyst", "product+analyst"]
-title_test = ["data+scientist"]
-city_test = ["Los+Angeles"]
-city_set = ["San+Francisco", "Seattle", "Los+Angeles", "New+York", "Boston"]
-columns = ["city", "company_name", "category", "job_title", "qualified", "tech_compatible", "major_compatible", "app_link", "indeed_link"]
-df = pd.DataFrame(columns = columns)
+title_set = ['data+scientist', 'data+analyst', 'product+analyst']
+title_test = ['data+scientist']
+city_test = ['Los+Angeles']
+city_set = ['San+Francisco', 'Seattle', 'Los+Angeles', 'New+York', 'Boston']
+columns = ['city', 'company_name', 'category', 'job_title', 'qualified',
+           'tech_compatible', 'major_compatible', 'app_link', 'indeed_link']
+
+start_range = range(0, 100, 10)
 
 
-for title in title_test:
-	for city in city_test:
-		for start in range(0, max_results, 10):
-			jobs, postings, app_links, qualified, plang, majors, companies = [], [], [], [], [], [], []
-			page = requests.get("https://www.indeed.com/jobs?as_and=" + title + 
-					   "&as_phr=&as_any=bachelor%2C+BA&as_not=&as_ttl=&as_cmp=&jt=all&st=&as_src=&salary=&radius=50&l="+ city + 
-					   "&fromage=any&limit=10&sort=&psf=advsrch&start=" + str(start))
-			time.sleep(5)
-			soup = BeautifulSoup(page.text, "html.parser")
+def format_job_link(title, city, start):
+    link = f'https://www.indeed.com/jobs?as_and=' \
+           f'{title}&as_phr=&as_any=bachelor%2CBA&as_not=&as_ttl=&as_cmp=&jt' \
+           f'=all&st=&as_src=&salary=&radius=50&l=' \
+           f'{city}&fromage=any&limit=10&sort=&psf=advsrch&start={str(start)}'
+    return link
 
-			for div in soup.find_all(name="div", attrs={"class":"row"}):
-				# Company names
-				company = div.find_all(name="span", attrs={"class":"company"})
-				if len(company) > 0:
-					companies.append(company[0].text.strip())
-				else:
-					sec_try = div.find_all(name="span", attrs={"class":"result-link-source"})
-					for span in sec_try:
-						companies.append(span.text.strip())
 
-				for a in div.find_all(name="a", attrs={"data-tn-element":"jobTitle"}):
-					# Jobs titles
-					jobs.append("".join(a.text.strip()))
-					postings.append("https://indeed.com" + a["href"])
+df = pd.DataFrame(columns=columns)
 
-					# Job listing detail page
-					job_page = requests.get("https://indeed.com" + a["href"])
-					time.sleep(5)
-					job_soup = BeautifulSoup(job_page.text, "html.parser")
+for title, city, start in itertools.product(title_test, city_test,
+                                            start_range):
+    jobs, postings, app_links, qualified, lang, majors, companies = \
+        [], [], [], [], [], [], []
 
-					# Application link
-					application_links = job_soup.find_all(name = "a", attrs = {"class":"icl-Button icl-Button--primary icl-Button--md"})
-					application_links.sort(key = lambda x: x["href"], reverse = True)
-					if application_links:
-						for link in application_links:
-							if link["href"].find("promo/resume") == -1:
-								app_links.append(link["href"])
-								break
-							else:
-								app_links.append("https://indeed.com" + a["href"])
-					else:
-						app_links.append("https://indeed.com" + a["href"])
+    page = requests.get(format_job_link(title, city, start))
+    time.sleep(5)
+    soup = BeautifulSoup(page.text, 'html.parser')
 
-					# Whether within 2 YOE and Bachlor
-					data = str(job_soup.findAll(text = True)).lower()
-					if (re.search("(bachelor|ba)[^a-z0-9]", data) is not None or re.search("(master|ma|phd)[^a-z0-9]", data) is None):
-						if re.search("[1-2].{1,10}(Y|y)ear", data) is not None:
-							qualified.append("True")
-						else:
-							qualified.append("Need YOE")
-					elif re.search("[1-2].{1,10}(Y|y)ear", data) is not None:
-						qualified.append("Need Advanced Degree")
-					else:
-						qualified.append("False")
+    for div in soup.find_all(name='div', attrs={'class': 'row'}):
+        # Company names
+        company = div.find_all(name='span', attrs={'class': 'company'})
+        if len(company) > 0:
+            companies.append(company[0].text.strip())
+        else:
+            sec_try = div.find_all(name='span', attrs={
+                'class': 'result-link-source'})
+            for span in sec_try:
+                companies.append(span.text.strip())
 
-					# Compatible technologies & major
-					plang.append("True") if re.search("(r|sql|python)[^a-z0-9]", data) is not None else plang.append("False")
-					majors.append("True") if re.search("(economics)[^a-z0-9]", data) is not None else majors.append("False")
+        # here we go through each job listing
+        for a in div.find_all(name='a', attrs={'data-tn-element': 'jobTitle'}):
+            # Jobs titles
+            jobs.append(''.join(a.text.strip()))
+            postings.append('https://indeed.com' + a['href'])
 
-			listings = [("job_title", jobs),
-						("company_name", companies),
-						("indeed_link", postings), 
-						("app_link", app_links), 
-						("qualified", qualified), 
-						("tech_compatible", plang), 
-						("major_compatible", majors)]
-			temp_df = pd.DataFrame.from_dict(dict(listings))
-			temp_df = temp_df.assign(city = str(city))
-			temp_df = temp_df.assign(category = str(title))
-			print(temp_df)
-			df = df.append(temp_df, ignore_index = True)
+            # Job listing detail page
+            job_page = requests.get('https://indeed.com' + a['href'])
+            time.sleep(5)
 
-df = df[df.qualified == "True"]
-df = df.drop_duplicates(subset = ["city", "company_name", "category", "job_title", "qualified", "tech_compatible", "major_compatible", "app_link"])
-df = df[~df.job_title.str.lower().str.contains("senior|manager|lead|staff|head|sr |financ|business analyst|brand analyst|specialist|engineer|designer|intern|contract")]
-df = df[["city", "company_name", "category", "job_title", "qualified", "tech_compatible", "major_compatible", "app_link", "indeed_link"]]
-df = df.sort_values(by = ["qualified", "tech_compatible", "major_compatible", "city", "company_name", "category"], ascending = [0, 0, 0, 0, 0, 1])
-df.to_csv(encoding="utf-8", index=False)
+            job_soup = BeautifulSoup(job_page.text, 'html.parser')
+
+            # todo get the entire job description
+
+            # todo take out the stop words
+
+            # todo tokenize
+
+            # todo find tf-idf
+
+            # todo extract keywords and add it as a separate column
+
+            # keywords should serve as a way to filter more easily
+
+            # Application link
+            application_links = job_soup.find_all(name='a', attrs={
+                'class': 'icl-Button icl-Button--primary ' 'icl-Button--md'})
+            application_links.sort(key=lambda x: x['href'], reverse=True)
+
+            if application_links:
+                for link in application_links:
+                    if link['href'].find('promo/resume') == -1:
+                        app_links.append(link['href'])
+                        break
+                    else:
+                        app_links.append('https://indeed.com' + a['href'])
+            else:
+                app_links.append('https://indeed.com' + a['href'])
+
+            # Whether within 2 YOE and Bachlor
+            data = str(job_soup.findAll(text=True)).lower()
+            if (re.search('(bachelor|ba)[^a-z0-9]',
+                          data) is not None or re.search(
+                '(master|ms|ma|phd)[^a-z0-9]', data) is None):
+                if re.search('[1-2].{1,10}(Y|y)ear', data) is not None:
+                    qualified.append('True')
+                else:
+                    qualified.append('Need YOE')
+            elif re.search('[1-2].{1,10}(Y|y)ear', data) is not None:
+                qualified.append('Need Advanced Degree')
+            else:
+                qualified.append('False')
+
+            # Compatible language
+            if re.search('(r|sql|python)[^a-z0-9]', data) is not None:
+                lang.append('True')
+            else:
+                lang.append('False')
+
+            if re.search('(economics)[^a-z0-9]', data) is not None:
+                majors.append('True')
+            else:
+                majors.append('False')
+
+    listings = [('job_title', jobs),
+                ('company_name', companies),
+                ('indeed_link', postings),
+                ('app_link', app_links),
+                ('qualified', qualified),
+                ('tech_compatible', lang),
+                ('major_compatible', majors)]
+    temp_df = pd.DataFrame.from_dict(dict(listings))
+    temp_df = temp_df.assign(city=str(city))
+    temp_df = temp_df.assign(category=str(title))
+    df = df.append(temp_df, ignore_index=True)
+
+df = df[df.qualified == 'True']
+df = df.drop_duplicates(
+    subset=['city', 'company_name', 'category', 'job_title', 'qualified',
+            'tech_compatible', 'major_compatible', 'app_link'])
+df = df[~df.job_title.str.lower().str.contains(
+    'senior|manager|lead|staff|head|sr |financ|business analyst|brand '
+    'analyst|specialist|engineer|designer|intern|contract')]
+df = df[['city', 'company_name', 'category', 'job_title', 'qualified',
+         'tech_compatible', 'major_compatible', 'app_link', 'indeed_link']]
+df = df.sort_values(
+    by=['qualified', 'tech_compatible', 'major_compatible', 'city',
+        'company_name', 'category'], ascending=[0, 0, 0, 0, 0, 1])
+df.to_csv('output.csv', encoding='utf-8', index=False)
+
+# recommend jobs based on user's rating (outcome)
+# as they provide more rating, the model performance will improve
+# input features can be a variety of factors
+
+"""
+# design for personalization method over time
+
+OUTPUT:
+user rating (likert scale 1-7)
+
+INPUT:
+description-char
+- keywords
+- sentiment of the job description
+
+job-char
+- locations
+- industry
+
+company-char
+- current rating of the company on glassdoor/indeed
+- match w/ the candidate's current profile
+"""
+
+# todo covert keywords (or their embeddings) as features
+
+# todo predict relationship between keywords and user's listing preference
 
